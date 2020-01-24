32e2fe0e6e7b9923c28efe1d53e927eb50a478db
shenzako <57211070+shenzako@users.noreply.github.com>
2020-01-24 13:18:45 +0400

    Ver 2 Kilapoze avec rajout d'un 2eme message

---
+++ Kila_pose/src/kilapoze.java
@@ -0,0 +1,19 @@
+/* programme qui affiche le message « Kila Poze Korek ?»
+   auteur : Tinky Winky
+   date : 27/08/2019
+*/
+
+
+public class kilapoze {
+
+	public static void main(String[] args) {
+		// affiche le message Ki la poze 
+	    
+		System.out.println("KILAPOZE KOREK ?");
+		
+		// Test EGIT on modifie le code initial
+		
+		System.out.println("OUI ! DANS BIEN EK EGIT");
+	}
+
+}
